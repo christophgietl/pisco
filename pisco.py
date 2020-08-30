b39f35dce825297dd2815193ca5fd0e9835b769d
Christoph Gietl <christophgietl@users.noreply.github.com>
2020-08-30 13:29:21 +0200

    Refactor pisco.py

--- pisco.py
+++ pisco.py
@@ -1,5 +1,7 @@
 #! /usr/bin/env python3
 
+import contextlib
+import functools
 import io
 import logging.config
 import queue
@@ -17,7 +19,6 @@ import xdg
 
 AFTER_MS = 40
 BACKLIGHT_DIRECTORY = Path("/sys/class/backlight/soc:backlight/")
-DEVICE_NAME = "Schlafzimmer"
 LOG_DIRECTORY = xdg.XDG_DATA_HOME / "pisco" / "logs"
 LOGGING = {
     "disable_existing_loggers": False,
@@ -36,6 +37,7 @@ LOGGING = {
     "root": {"handlers": ["rot_file_handler", "stream_handler"], "level": "INFO"},
     "version": 1
 }
+SONOS_DEVICE_NAME = "Schlafzimmer"
 WINDOW_HEIGHT = 320
 WINDOW_WIDTH = 240
 
@@ -44,88 +46,64 @@ logging.config.dictConfig(LOGGING)
 logger = logging.getLogger(__name__)
 
 
-class Backlight:
-    def __init__(self, backlight_directory: Path):
-        self.backlight_directory = backlight_directory
-        self.brightness = backlight_directory / "brightness"
-        self.max_brightness = backlight_directory / "max_brightness"
+class Backlight(contextlib.AbstractContextManager):
+    def __exit__(self, exc_type, exc_value, traceback) -> None:
+        logger.info(f"Tearing down interface to backlight {self._backlight_directory} ...")
+        self.activate()
+        logger.info(f"Interface to backlight {self._backlight_directory} torn down.")
+
+    def __init__(self, backlight_directory: Path) -> None:
+        logger.info(f"Initialising interface to backlight {backlight_directory} ...")
+        self._backlight_directory = backlight_directory
+        self._brightness = backlight_directory / "brightness"
+        self._max_brightness = backlight_directory / "max_brightness"
+        logger.info(f"Interface to backlight {backlight_directory} initialised.")
 
     def activate(self) -> None:
+        logger.info(f"Activating backlight {self._backlight_directory} ...")
         try:
-            max_brightness_value = self.max_brightness.read_text()
-            self.brightness.write_text(max_brightness_value)
+            max_brightness_value = self._max_brightness.read_text()
+            self._brightness.write_text(max_brightness_value)
         except OSError:
-            logger.exception(f"Failed to activate backlight {self.backlight_directory}.")
+            logger.exception(f"Could not activate backlight {self._backlight_directory}.")
+        else:
+            logger.info(f"Backlight {self._backlight_directory} activated.")
 
     def deactivate(self) -> None:
+        logger.info(f"Deactivating backlight {self._backlight_directory} ...")
         try:
-            self.brightness.write_text("0")
+            self._brightness.write_text("0")
         except OSError:
-            logger.exception(f"Failed to deactivate backlight {self.backlight_directory}.")
+            logger.exception(f"Could not deactivate backlight {self._backlight_directory}.")
+        else:
+            logger.info(f"Backlight {self._backlight_directory} deactivated.")
 
 
-class AlbumArtLabel(tkinter.Label):
-    def __init__(self, av_transport_event_queue: queue.Queue, backlight: Backlight, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.av_transport_event_queue = av_transport_event_queue
-        self.backlight = backlight
-        self.album_art_absolute_uri = ""
-        self.album_art_image = self.get_tk_photo_image(self.album_art_absolute_uri)
-        self.after(AFTER_MS, self.process_av_transport_event_queue)
+class HttpPhotoImageManager:
+    @classmethod
+    def _create_photo_image(cls, absolute_uri: str) -> PIL.ImageTk.PhotoImage:
+        logger.info(f"Creating Tkinter-compatible photo image from URI {absolute_uri} ...")
+        content = cls._download_resource(absolute_uri)
+        image: PIL.Image.Image = PIL.Image.open(io.BytesIO(content))
+        image_wo_alpha = cls._remove_alpha_channel(image)
+        resized_image = cls._resize_image(image_wo_alpha, WINDOW_WIDTH, WINDOW_HEIGHT)
+        photo_image = PIL.ImageTk.PhotoImage(resized_image)
+        logger.info(f"Tkinter-compatible photo image created from URI {absolute_uri}.")
+        return photo_image
 
     @staticmethod
-    def download_resource(uri: str) -> bytes:
-        logger.info(f"Downloading resource {uri} ...")
-        r = requests.get(uri)
+    def _download_resource(absolute_uri: str) -> bytes:
+        logger.info(f"Downloading resource {absolute_uri} ...")
+        r = requests.get(absolute_uri)
         content = r.content
-        logger.info(f"Resource {uri} downloaded.")
+        logger.info(f"Resource {absolute_uri} downloaded.")
         return content
 
-    @classmethod
-    def get_tk_photo_image(cls, album_art_absolute_uri: str) -> Optional[PIL.ImageTk.PhotoImage]:
-        if album_art_absolute_uri == "":
-            return None
-        content = cls.download_resource(album_art_absolute_uri)
-        image: PIL.Image.Image = PIL.Image.open(io.BytesIO(content))
-        image_wo_alpha = cls.remove_alpha_channel(image)
-        resized_image = cls.resize_image(image_wo_alpha, WINDOW_WIDTH, WINDOW_HEIGHT)
-        tk_photo_image = PIL.ImageTk.PhotoImage(resized_image)
-        return tk_photo_image
-
-    def process_av_transport_event(self, event: soco.events_base.Event) -> None:
-        logger.info(f"Processing AV transport event {event.__dict__} ...")
-        if event.variables["transport_state"] in ("PLAYING", "TRANSITIONING"):
-            self.process_track_meta_data(event)
-            self.backlight.activate()
-        else:
-            self.backlight.deactivate()
-            self.update_album_art("")
-        logger.info("AV transport event processed.")
-
-    def process_av_transport_event_queue(self) -> None:
-        try:
-            event = self.av_transport_event_queue.get_nowait()
-        except queue.Empty:
-            pass
-        else:
-            self.process_av_transport_event(event)
-        finally:
-            self.after(AFTER_MS, self.process_av_transport_event_queue)
-
-    def process_track_meta_data(self, event: soco.events_base.Event) -> None:
-        track_meta_data = event.variables["current_track_meta_data"]
-        logger.info(f"Processing track meta data {track_meta_data.__dict__} ...")
-        if hasattr(track_meta_data, "album_art_uri"):
-            album_art_uri = track_meta_data.album_art_uri
-            album_art_absolute_uri = event.service.soco.music_library.build_album_art_full_uri(album_art_uri)
-            self.update_album_art(album_art_absolute_uri)
-        logger.info("Track meta data processed.")
-
     @staticmethod
-    def remove_alpha_channel(image: PIL.Image.Image) -> PIL.Image.Image:
+    def _remove_alpha_channel(image: PIL.Image.Image) -> PIL.Image.Image:
         logger.info("Removing alpha channel ...")
         if image.mode != "RGBA":
-            logger.info("Image does not have an alpha channel.")
+            logger.info("Cannot remove alpha channel: Image does not have an alpha channel.")
             return image
         rgb_image = PIL.Image.new("RGB", image.size, "white")
         rgb_image.paste(image, mask=image.getchannel("A"))
@@ -133,7 +111,7 @@ class AlbumArtLabel(tkinter.Label):
         return rgb_image
 
     @staticmethod
-    def resize_image(image: PIL.Image.Image, max_width: int, max_height: int) -> PIL.Image.Image:
+    def _resize_image(image: PIL.Image.Image, max_width: int, max_height: int) -> PIL.Image.Image:
         logger.info("Resizing image ...")
         if max_width * image.height <= max_height * image.width:
             new_width = max_width
@@ -145,102 +123,146 @@ class AlbumArtLabel(tkinter.Label):
         logger.info("Image resized.")
         return resized_image
 
-    def update_album_art(self, album_art_absolute_uri: str) -> None:
+    @classmethod
+    @functools.lru_cache(maxsize=1)
+    def get_photo_image(cls, absolute_uri: Optional[str]) -> Optional[PIL.ImageTk.PhotoImage]:
+        if absolute_uri is not None:
+            photo_image = cls._create_photo_image(absolute_uri)
+            return photo_image
+
+
+class PlaybackInformationLabel(tkinter.Label):
+    def __init__(self, av_transport_event_queue: queue.Queue, backlight: Backlight, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+        self._av_transport_event_queue = av_transport_event_queue
+        self._backlight = backlight
+        self._album_art_image_manager = HttpPhotoImageManager()
+        self.after(AFTER_MS, self._process_av_transport_event_queue)
+
+    def _process_av_transport_event(self, event: soco.events_base.Event) -> None:
+        logger.info(f"Processing AV transport event {event.__dict__} ...")
+        if event.variables["transport_state"] in ("PLAYING", "TRANSITIONING"):
+            self._process_track_meta_data(event)
+            self._backlight.activate()
+        else:
+            self._backlight.deactivate()
+            self._update_album_art(None)
+        logger.info("AV transport event processed.")
+
+    def _process_av_transport_event_queue(self) -> None:
+        try:
+            event = self._av_transport_event_queue.get_nowait()
+        except queue.Empty:
+            pass
+        else:
+            self._process_av_transport_event(event)
+        finally:
+            self.after(AFTER_MS, self._process_av_transport_event_queue)
+
+    def _process_track_meta_data(self, event: soco.events_base.Event) -> None:
+        track_meta_data = event.variables["current_track_meta_data"]
+        logger.info(f"Processing track meta data {track_meta_data.__dict__} ...")
+        if hasattr(track_meta_data, "album_art_uri"):
+            album_art_uri = track_meta_data.album_art_uri
+            album_art_absolute_uri = event.service.soco.music_library.build_album_art_full_uri(album_art_uri)
+            self._update_album_art(album_art_absolute_uri)
+        logger.info("Track meta data processed.")
+
+    def _update_album_art(self, absolute_uri: Optional[str]) -> None:
         logger.info("Updating album art ...")
-        if self.album_art_absolute_uri == album_art_absolute_uri:
-            logger.info("Album art is already up-to-date.")
-            return
-        self.album_art_image = self.get_tk_photo_image(album_art_absolute_uri)
-        self.config(image=self.album_art_image)
-        self.album_art_absolute_uri = album_art_absolute_uri
+        album_art_photo_image = self._album_art_image_manager.get_photo_image(absolute_uri)
+        self.config(image=album_art_photo_image)
         logger.info("Album art updated.")
 
 
-class App(tkinter.Tk):
-    def __init__(self, device: soco.core.SoCo, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.device = device
-        self.geometry(f"{WINDOW_WIDTH}x{WINDOW_HEIGHT}")
-        self.title("Pisco")
-        self.bind_all("<KeyPress>", self.handle_key_press_event)
-        signal.signal(signal.SIGINT, self.handle_int_or_term_signal)
-        signal.signal(signal.SIGTERM, self.handle_int_or_term_signal)
+class SonosDevice(contextlib.AbstractContextManager):
+    def __exit__(self, exc_type, exc_value, traceback) -> None:
+        logger.info(f"Tearing down interface to Sonos device {self.controller.player_name} ...")
+        self._av_transport_subscription.unsubscribe()
+        self._av_transport_subscription.event_listener.stop()
+        logger.info(f"Interface to Sonos device {self.controller.player_name} torn down.")
 
-    def handle_int_or_term_signal(self, signal_number: int, _) -> None:
-        logger.info(f"Received signal {signal_number}.")
-        self.destroy()
-
-    def handle_key_press_event(self, event: tkinter.Event) -> None:
-        logger.info(f"Handling key press event {event} ...")
-        # noinspection PyUnresolvedReferences
-        key_symbol: str = event.keysym
-        if key_symbol.isdigit():
-            self.play_sonos_favorite(int(key_symbol))
-        elif key_symbol in ("Left", "XF86AudioRewind"):
-            self.device.previous()
-        elif key_symbol in ("Right", "XF86AudioForward"):
-            self.device.next()
-        elif key_symbol in ("Return", "XF86AudioPlay"):
-            self.toggle_current_transport_state()
-        elif key_symbol == "XF86AudioStop":  # not supported by Rii MX6
-            self.device.stop()
-        elif key_symbol == "XF86AudioMute":
-            self.device.mute = not self.device.mute
-        elif key_symbol in ("Up", "XF86AudioRaiseVolume"):
-            self.device.set_relative_volume(+5)
-        elif key_symbol in ("Down", "XF86AudioLowerVolume"):
-            self.device.set_relative_volume(-5)
-        else:
-            logger.info(f"No action defined for key press {event}.")
-        logger.info(f"Key press event {event} handled.")
+    def __init__(self, name: str) -> None:
+        logger.info(f"Initialising interface to Sonos device {name} ...")
+        self.controller: soco.core.SoCo = soco.discovery.by_name(name)
+        self._av_transport_subscription: soco.events.Subscription = self.controller.avTransport.subscribe()
+        self.av_transport_event_queue: queue.Queue = self._av_transport_subscription.events
+        logger.info(f"Interface to Sonos device {name} initialised.")
 
     def play_sonos_favorite(self, favorite_index: int) -> None:
         logger.info(f"Starting to play Sonos favorite {favorite_index} ...")
-        favorite = self.device.music_library.get_sonos_favorites()[favorite_index]
+        favorite = self.controller.music_library.get_sonos_favorites()[favorite_index]
         favorite_uri = favorite.resources[0].uri
         favorite_meta_data = favorite.resource_meta_data
-        self.device.play_uri(favorite_uri, favorite_meta_data)
+        self.controller.play_uri(favorite_uri, favorite_meta_data)
         logger.info(f"Started to play Sonos favorite {favorite_index}.")
 
     def toggle_current_transport_state(self) -> None:
         logger.info("Toggling current transport state ...")
-        transport = self.device.get_current_transport_info()
+        transport = self.controller.get_current_transport_info()
         state = transport["current_transport_state"]
         if state == "PLAYING":
-            self.device.pause()
+            self.controller.pause()
         else:
-            self.device.play()
+            self.controller.play()
         logger.info("Toggled current transport state.")
 
 
-def clean_up(av_transport_subscription: soco.events.Subscription, backlight: Backlight) -> None:
-    logger.info("Cleaning up ...")
-    av_transport_subscription.unsubscribe()
-    av_transport_subscription.event_listener.stop()
-    backlight.activate()
-    logger.info("Cleaned up.")
+class UserInterface(tkinter.Tk):
+    def __init__(self, sonos_device: SonosDevice, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+        self._sonos_device = sonos_device
+        self.geometry(f"{WINDOW_WIDTH}x{WINDOW_HEIGHT}")
+        self.title("Pisco")
+        self.bind_all("<KeyPress>", self._handle_key_press_event)
+        signal.signal(signal.SIGINT, self._handle_int_or_term_signal)
+        signal.signal(signal.SIGTERM, self._handle_int_or_term_signal)
+
+    def _handle_int_or_term_signal(self, signal_number: int, _) -> None:
+        logger.info(f"Handling signal {signal_number} ...")
+        self.destroy()
+        logger.info(f"Signal {signal_number} handled.")
+
+    def _handle_key_press_event(self, event: tkinter.Event) -> None:
+        logger.info(f"Handling key press event {event} ...")
+        # noinspection PyUnresolvedReferences
+        key_symbol: str = event.keysym
+        device = self._sonos_device
+        if key_symbol.isdigit():
+            device.play_sonos_favorite(int(key_symbol))
+        elif key_symbol in ("Left", "XF86AudioRewind"):
+            device.controller.previous()
+        elif key_symbol in ("Right", "XF86AudioForward"):
+            device.controller.next()
+        elif key_symbol in ("Return", "XF86AudioPlay"):
+            device.toggle_current_transport_state()
+        elif key_symbol == "XF86AudioStop":  # not supported by Rii MX6
+            device.controller.stop()
+        elif key_symbol == "XF86AudioMute":
+            device.controller.mute = not device.controller.mute
+        elif key_symbol in ("Up", "XF86AudioRaiseVolume"):
+            device.controller.set_relative_volume(+5)
+        elif key_symbol in ("Down", "XF86AudioLowerVolume"):
+            device.controller.set_relative_volume(-5)
+        else:
+            logger.info(f"No action defined for key press {event}.")
+        logger.info(f"Key press event {event} handled.")
 
 
 def main() -> None:
-    device: soco.core.SoCo = soco.discovery.by_name(DEVICE_NAME)
-    av_transport_subscription: soco.events.Subscription = device.avTransport.subscribe()
-    av_transport_event_queue: queue.Queue = av_transport_subscription.events
-
-    backlight = Backlight(BACKLIGHT_DIRECTORY)
-
-    app = App(device)
-    label = AlbumArtLabel(
-        master=app,
-        background="black",
-        av_transport_event_queue=av_transport_event_queue,
-        backlight=backlight
-    )
-    label.pack(expand=True, fill="both")
-
-    try:
-        app.mainloop()
-    finally:
-        clean_up(av_transport_subscription, backlight)
+    with SonosDevice(SONOS_DEVICE_NAME) as sonos_device:
+        with Backlight(BACKLIGHT_DIRECTORY) as backlight:
+            logger.info("Running pisco user interface ...")
+            user_interface = UserInterface(sonos_device)
+            playback_information_label = PlaybackInformationLabel(
+                master=user_interface,
+                background="black",
+                av_transport_event_queue=sonos_device.av_transport_event_queue,
+                backlight=backlight
+            )
+            playback_information_label.pack(expand=True, fill="both")
+            user_interface.mainloop()
+            logger.info("Pisco user interface run.")
 
 
 if __name__ == "__main__":
