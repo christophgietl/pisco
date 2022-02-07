7987c9d08dec060672d620ddd19b8973c7656081
Christoph Gietl <christophgietl@users.noreply.github.com>
2022-02-07 21:04:37 +0100

    Use black for code formatting

--- src/pisco.py
+++ src/pisco.py
@@ -21,12 +21,13 @@ import xdg
 
 log_directory = xdg.XDG_DATA_HOME / "pisco" / "logs"
 log_directory.mkdir(exist_ok=True, parents=True)
+log_format = "%(asctime)s %(name)s %(levelname)s %(message)s %(thread)s %(threadName)s"
 logging_configuration = {
     "disable_existing_loggers": False,
     "formatters": {
         "json_formatter": {
             "class": "pythonjsonlogger.jsonlogger.JsonFormatter",
-            "format": "%(asctime)s - %(name)s - %(levelname)s - %(message)s - %(thread)s - %(threadName)s"
+            "format": log_format,
         }
     },
     "handlers": {
@@ -35,11 +36,11 @@ logging_configuration = {
             "class": "logging.handlers.RotatingFileHandler",
             "filename": log_directory / "pisco.jsonl",
             "formatter": "json_formatter",
-            "maxBytes": 1_000_000
+            "maxBytes": 1_000_000,
         }
     },
     "root": {"handlers": ["rot_file_handler"], "level": "DEBUG"},
-    "version": 1
+    "version": 1,
 }
 logging.config.dictConfig(logging_configuration)
 logger = logging.getLogger(__name__)
@@ -47,57 +48,75 @@ logger = logging.getLogger(__name__)
 
 class Backlight(ContextManager["Backlight"]):
     def __exit__(
-            self,
-            exc_type: Optional[Type[BaseException]],
-            exc_value: Optional[BaseException],
-            traceback: Optional[TracebackType]
+        self,
+        exc_type: Optional[Type[BaseException]],
+        exc_value: Optional[BaseException],
+        traceback: Optional[TracebackType],
     ) -> None:
         if self._backlight_directory:
             logger.info(
                 "Tearing down interface to backlight ...",
-                extra={"backlight_directory": self._backlight_directory}
+                extra={"backlight_directory": self._backlight_directory},
             )
             self.activate()
             logger.info(
                 "Interface to backlight torn down.",
-                extra={"backlight_directory": self._backlight_directory}
+                extra={"backlight_directory": self._backlight_directory},
             )
 
     def __init__(self, backlight_directory: Optional[str]) -> None:
         self._backlight_directory = None
         if backlight_directory:
-            logger.info("Initializing interface to backlight ...", extra={"backlight_directory": backlight_directory})
+            logger.info(
+                "Initializing interface to backlight ...",
+                extra={"backlight_directory": backlight_directory},
+            )
             self._backlight_directory = Path(backlight_directory)
             self._brightness = self._backlight_directory / "brightness"
             self._max_brightness = self._backlight_directory / "max_brightness"
-            logger.info("Interface to backlight initialized.", extra={"backlight_directory": backlight_directory})
+            logger.info(
+                "Interface to backlight initialized.",
+                extra={"backlight_directory": backlight_directory},
+            )
 
     def activate(self) -> None:
         if self._backlight_directory:
-            logger.info("Activating backlight ...", extra={"backlight_directory": self._backlight_directory})
+            logger.info(
+                "Activating backlight ...",
+                extra={"backlight_directory": self._backlight_directory},
+            )
             try:
                 max_brightness_value = self._max_brightness.read_text()
                 self._brightness.write_text(max_brightness_value)
             except OSError:
                 logger.exception(
                     "Could not activate backlight.",
-                    extra={"backlight_directory": self._backlight_directory}
+                    extra={"backlight_directory": self._backlight_directory},
                 )
             else:
-                logger.info("Backlight activated.", extra={"backlight_directory": self._backlight_directory})
+                logger.info(
+                    "Backlight activated.",
+                    extra={"backlight_directory": self._backlight_directory},
+                )
 
     def deactivate(self) -> None:
         if self._backlight_directory:
-            logger.info("Deactivating backlight ...", extra={"backlight_directory": self._backlight_directory})
+            logger.info(
+                "Deactivating backlight ...",
+                extra={"backlight_directory": self._backlight_directory},
+            )
             try:
                 self._brightness.write_text("0")
             except OSError:
                 logger.exception(
                     "Could not deactivate backlight.",
-                    extra={"backlight_directory": self._backlight_directory}
+                    extra={"backlight_directory": self._backlight_directory},
                 )
             else:
-                logger.info("Backlight deactivated.", extra={"backlight_directory": self._backlight_directory})
+                logger.info(
+                    "Backlight deactivated.",
+                    extra={"backlight_directory": self._backlight_directory},
+                )
 
 
 class HttpPhotoImageManager:
@@ -117,7 +136,9 @@ class HttpPhotoImageManager:
     def _remove_alpha_channel(image: PIL.Image.Image) -> PIL.Image.Image:
         logger.debug("Removing alpha channel ...")
         if image.mode != "RGBA":
-            logger.debug("Cannot remove alpha channel: Image does not have an alpha channel.")
+            logger.debug(
+                "Cannot remove alpha channel: Image does not have an alpha channel."
+            )
             return image
         rgb_image = PIL.Image.new("RGB", image.size, "white")
         rgb_image.paste(image, mask=image.getchannel("A"))
@@ -138,26 +159,32 @@ class HttpPhotoImageManager:
 
     @functools.lru_cache(maxsize=1)
     def get_photo_image(self, absolute_uri: str) -> PIL.ImageTk.PhotoImage:
-        logger.debug("Creating Tkinter-compatible photo image ...", extra={"URI": absolute_uri})
+        logger.debug(
+            "Creating Tkinter-compatible photo image ...",
+            extra={"URI": absolute_uri},
+        )
         content = self._download_resource(absolute_uri)
         image = PIL.Image.open(io.BytesIO(content))
         image_wo_alpha = self._remove_alpha_channel(image)
         resized_image = self._resize_image(image_wo_alpha)
         photo_image = PIL.ImageTk.PhotoImage(resized_image)
-        logger.debug("Tkinter-compatible photo image created.", extra={"URI": absolute_uri})
+        logger.debug(
+            "Tkinter-compatible photo image created.",
+            extra={"URI": absolute_uri},
+        )
         return photo_image
 
 
 class PlaybackInformationLabel(tkinter.Label):
     def __init__(
-            self,
-            av_transport_event_queue: queue.Queue[soco.events_base.Event],
-            backlight: Backlight,
-            max_width: int,
-            max_height: int,
-            refresh_interval: int,
-            *args: Any,
-            **kwargs: Any
+        self,
+        av_transport_event_queue: queue.Queue[soco.events_base.Event],
+        backlight: Backlight,
+        max_width: int,
+        max_height: int,
+        refresh_interval: int,
+        *args: Any,
+        **kwargs: Any,
     ) -> None:
         super().__init__(*args, **kwargs)
         self._av_transport_event_queue = av_transport_event_queue
@@ -167,7 +194,10 @@ class PlaybackInformationLabel(tkinter.Label):
         self.after(self._refresh_interval, self._process_av_transport_event_queue)
 
     def _process_av_transport_event(self, event: soco.events_base.Event) -> None:
-        logger.info("Processing AV transport event ...", extra={"event": event.__dict__})
+        logger.info(
+            "Processing AV transport event ...",
+            extra={"event": event.__dict__},
+        )
         if event.variables["transport_state"] in ("PLAYING", "TRANSITIONING"):
             self._process_track_meta_data(event)
             self._backlight.activate()
@@ -188,17 +218,27 @@ class PlaybackInformationLabel(tkinter.Label):
 
     def _process_track_meta_data(self, event: soco.events_base.Event) -> None:
         track_meta_data = event.variables["current_track_meta_data"]
-        logger.info("Processing track meta data ...", extra={"track_meta_data": track_meta_data.__dict__})
+        logger.info(
+            "Processing track meta data ...",
+            extra={"track_meta_data": track_meta_data.__dict__},
+        )
         if hasattr(track_meta_data, "album_art_uri"):
             album_art_uri = track_meta_data.album_art_uri
-            album_art_absolute_uri = event.service.soco.music_library.build_album_art_full_uri(album_art_uri)
+            album_art_absolute_uri = (
+                event.service.soco.music_library.build_album_art_full_uri(album_art_uri)
+            )
             self._update_album_art(album_art_absolute_uri)
-        logger.info("Track meta data processed.", extra={"track_meta_data": track_meta_data.__dict__})
+        logger.info(
+            "Track meta data processed.",
+            extra={"track_meta_data": track_meta_data.__dict__},
+        )
 
     def _update_album_art(self, absolute_uri: Optional[str]) -> None:
         logger.info("Updating album art ...", extra={"URI": absolute_uri})
         if absolute_uri:
-            album_art_photo_image = self._album_art_image_manager.get_photo_image(absolute_uri)
+            album_art_photo_image = self._album_art_image_manager.get_photo_image(
+                absolute_uri
+            )
             self.config(image=album_art_photo_image)
         else:
             self.config(image="")  # removes image if present
@@ -207,25 +247,34 @@ class PlaybackInformationLabel(tkinter.Label):
 
 class SonosDevice(ContextManager["SonosDevice"]):
     def __exit__(
-            self,
-            exc_type: Optional[Type[BaseException]],
-            exc_value: Optional[BaseException],
-            traceback: Optional[TracebackType]
+        self,
+        exc_type: Optional[Type[BaseException]],
+        exc_value: Optional[BaseException],
+        traceback: Optional[TracebackType],
     ) -> None:
         logger.info(
             "Tearing down interface to Sonos device ...",
-            extra={"sonos_device_name": self.controller.player_name}
+            extra={"sonos_device_name": self.controller.player_name},
         )
         self._av_transport_subscription.unsubscribe()
         self._av_transport_subscription.event_listener.stop()
-        logger.info("Interface to Sonos device torn down.", extra={"sonos_device_name": self.controller.player_name})
+        logger.info(
+            "Interface to Sonos device torn down.",
+            extra={"sonos_device_name": self.controller.player_name},
+        )
 
     def __init__(self, name: str) -> None:
-        logger.info("Initializing interface to Sonos device ...", extra={"sonos_device_name": name})
+        logger.info(
+            "Initializing interface to Sonos device ...",
+            extra={"sonos_device_name": name},
+        )
         self.controller: soco.core.SoCo = soco.discovery.by_name(name)
-        self._av_transport_subscription: soco.events.Subscription = self._initialize_av_transport_subscription()
-        self.av_transport_event_queue: queue.Queue[soco.events_base.Event] = self._av_transport_subscription.events
-        logger.info("Interface to Sonos device initialized.", extra={"sonos_device_name": name})
+        self._av_transport_subscription = self._initialize_av_transport_subscription()
+        self.av_transport_event_queue = self._av_transport_subscription.events
+        logger.info(
+            "Interface to Sonos device initialized.",
+            extra={"sonos_device_name": name},
+        )
 
     def _initialize_av_transport_subscription(self) -> soco.events.Subscription:
         def handle_autorenew_failure(_: Exception) -> None:
@@ -242,12 +291,18 @@ class SonosDevice(ContextManager["SonosDevice"]):
         return subscription
 
     def play_sonos_favorite(self, favorite_index: int) -> None:
-        logger.info("Starting to play Sonos favorite ...", extra={"sonos_favorite_index": favorite_index})
+        logger.info(
+            "Starting to play Sonos favorite ...",
+            extra={"sonos_favorite_index": favorite_index},
+        )
         favorite = self.controller.music_library.get_sonos_favorites()[favorite_index]
         favorite_uri = favorite.resources[0].uri
         favorite_meta_data = favorite.resource_meta_data
         self.controller.play_uri(favorite_uri, favorite_meta_data)
-        logger.info("Started to play Sonos favorite.", extra={"sonos_favorite_index": favorite_index})
+        logger.info(
+            "Started to play Sonos favorite.",
+            extra={"sonos_favorite_index": favorite_index},
+        )
 
     def toggle_current_transport_state(self) -> None:
         logger.info("Toggling current transport state ...")
@@ -261,8 +316,14 @@ class SonosDevice(ContextManager["SonosDevice"]):
 
 
 class UserInterface(tkinter.Tk):
-    def __init__(self, sonos_device: SonosDevice, window_width: int, window_height: int, *args: Any, **kwargs: Any) \
-            -> None:
+    def __init__(
+        self,
+        sonos_device: SonosDevice,
+        window_width: int,
+        window_height: int,
+        *args: Any,
+        **kwargs: Any,
+    ) -> None:
         super().__init__(*args, **kwargs)
         self._sonos_device = sonos_device
         self.geometry(f"{window_width}x{window_height}")
@@ -297,52 +358,58 @@ class UserInterface(tkinter.Tk):
         elif key_symbol in ("Down", "XF86AudioLowerVolume"):
             device.controller.set_relative_volume(-5)
         else:
-            logger.info("No action defined for key press.", extra={"key_press_event": event})
+            logger.info(
+                "No action defined for key press.",
+                extra={"key_press_event": event},
+            )
         logger.info("Key press event handled.")
 
 
 @click.command()
-@click.argument('sonos_device_name')
+@click.argument("sonos_device_name")
 @click.option(
-    '-b',
-    '--backlight',
-    'backlight_directory',
-    help="sysfs directory of the backlight that should be deactivated when the device is not playing",
-    type=click.Path(exists=True, file_okay=False)
+    "-b",
+    "--backlight",
+    "backlight_directory",
+    help="""
+        sysfs directory of the backlight that should be deactivated
+        when the device is not playing
+    """,
+    type=click.Path(exists=True, file_okay=False),
 )
 @click.option(
-    '-w',
-    '--width',
-    'window_width',
+    "-w",
+    "--width",
+    "window_width",
     help="width of the Pisco window",
     type=click.IntRange(min=0),
     default=240,
-    show_default=True
+    show_default=True,
 )
 @click.option(
-    '-h',
-    '--height',
-    'window_height',
+    "-h",
+    "--height",
+    "window_height",
     help="height of the Pisco window",
     type=click.IntRange(min=0),
     default=320,
-    show_default=True
+    show_default=True,
 )
 @click.option(
-    '-r',
-    '--refresh',
-    'playback_information_refresh_interval',
+    "-r",
+    "--refresh",
+    "playback_information_refresh_interval",
     help="time in milliseconds after which playback information is updated",
     type=click.IntRange(min=1),
     default=40,
-    show_default=True
+    show_default=True,
 )
 def main(
-        sonos_device_name: str,
-        backlight_directory: Optional[str],
-        window_width: int,
-        window_height: int,
-        playback_information_refresh_interval: int
+    sonos_device_name: str,
+    backlight_directory: Optional[str],
+    window_width: int,
+    window_height: int,
+    playback_information_refresh_interval: int,
 ) -> None:
     """Control your Sonos device with your keyboard"""
     with SonosDevice(sonos_device_name) as sonos_device:
@@ -356,7 +423,7 @@ def main(
                 backlight=backlight,
                 max_width=window_width,
                 max_height=window_height,
-                refresh_interval=playback_information_refresh_interval
+                refresh_interval=playback_information_refresh_interval,
             )
             playback_information_label.pack(expand=True, fill="both")
             user_interface.mainloop()
--- tests/test_pisco.py
+++ tests/test_pisco.py
@@ -8,6 +8,7 @@ def test_pytest() -> None:
 class TestBacklight:
     def test_activate_passes_on_dummy_backlight(self) -> None:
         from pisco import Backlight
+
         backlight = Backlight(backlight_directory=None)
         backlight.activate()
 
@@ -16,20 +17,25 @@ class TestBacklight:
         brightness_file_path.write_text("19")
         max_brightness_file_path = tmp_path / "max_brightness"
         max_brightness_file_path.write_text("50")
+
         from pisco import Backlight
+
         backlight = Backlight(backlight_directory=str(tmp_path))
         backlight.activate()
         assert brightness_file_path.read_text() == max_brightness_file_path.read_text()
 
     def test_deactivate_passes_on_dummy_backlight(self) -> None:
         from pisco import Backlight
+
         backlight = Backlight(backlight_directory=None)
         backlight.deactivate()
 
     def test_deactivate_sets_brightness_to_0(self, tmp_path: Path) -> None:
         brightness_file_path = tmp_path / "brightness"
         brightness_file_path.write_text("19")
+
         from pisco import Backlight
+
         backlight = Backlight(backlight_directory=str(tmp_path))
         backlight.deactivate()
         assert brightness_file_path.read_text() == "0"
