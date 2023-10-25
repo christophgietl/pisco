e76ffc41d41fe2751f4a7f6132bed91022ff6286
Christoph Gietl <christophgietl@users.noreply.github.com>
2023-10-25 21:04:47 +0200

    Extract constant LOG_CONFIG

--- src/pisco/__init__.py
+++ src/pisco/__init__.py
@@ -14,25 +14,24 @@ LOG_FORMAT: Final[
     str
 ] = "%(asctime)s %(name)s %(levelname)s %(message)s %(thread)s %(threadName)s"
 
-logging.config.dictConfig(
-    {
-        "disable_existing_loggers": False,
-        "formatters": {
-            "json_formatter": {
-                "class": "pythonjsonlogger.jsonlogger.JsonFormatter",
-                "format": LOG_FORMAT,
-            }
-        },
-        "handlers": {
-            "rot_file_handler": {
-                "backupCount": 9,
-                "class": "logging.handlers.RotatingFileHandler",
-                "filename": LOG_FILE,
-                "formatter": "json_formatter",
-                "maxBytes": 1_000_000,
-            }
-        },
-        "root": {"handlers": ["rot_file_handler"], "level": "DEBUG"},
-        "version": 1,
-    }
-)
+LOG_CONFIG: Final[dict[str, object]] = {
+    "disable_existing_loggers": False,
+    "formatters": {
+        "json_formatter": {
+            "class": "pythonjsonlogger.jsonlogger.JsonFormatter",
+            "format": LOG_FORMAT,
+        }
+    },
+    "handlers": {
+        "rot_file_handler": {
+            "backupCount": 9,
+            "class": "logging.handlers.RotatingFileHandler",
+            "filename": LOG_FILE,
+            "formatter": "json_formatter",
+            "maxBytes": 1_000_000,
+        }
+    },
+    "root": {"handlers": ["rot_file_handler"], "level": "DEBUG"},
+    "version": 1,
+}
+logging.config.dictConfig(LOG_CONFIG)
