1bd310b803b53f9a67ac9fd507ce27b1d0d8c73e
Christoph Gietl <christophgietl@users.noreply.github.com>
2024-04-22 18:04:11 +0200

    Fix implementation of _discover_controller

--- src/pisco/input_output/sonos_device.py
+++ src/pisco/input_output/sonos_device.py
@@ -146,6 +146,6 @@ class SonosDeviceNotFoundError(Exception):
 
 
 def _discover_controller(name: str) -> soco.core.SoCo:
-    if controller := soco.discovery.by_name(name) is None:
+    if (controller := soco.discovery.by_name(name)) is None:
         raise SonosDeviceNotFoundError(name=name)
     return controller
